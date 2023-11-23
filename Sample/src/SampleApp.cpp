ab5199b3844240f3cdeb35e1d496746aebacf12f
monguri <mongry@gmail.com>
2023-11-23 19:35:36 +0900

    ライトベクトルの方向設定は本来の方向と逆方向で指定しているのでスポットライトのViewProjを作るときも反転して計算するように修正

--- Sample/src/SampleApp.cpp
+++ Sample/src/SampleApp.cpp
@@ -147,7 +147,10 @@ namespace
 		float outerAngle
 	)
 	{
-		const Matrix& spotLightShadowView = Matrix::CreateLookAt(pos, pos + dir * radius, Vector3::UnitY);
+		// lightVectorは逆方向で扱っているので符号反転
+		Vector3 normalizedDir = -dir;
+		normalizedDir.Normalize();
+		const Matrix& spotLightShadowView = Matrix::CreateLookAt(pos, pos + normalizedDir * radius, Vector3::UnitY);
 		const Matrix& spotLightShadowProj = Matrix::CreatePerspectiveFieldOfView(outerAngle * 2.0f, 1.0f, radius * 0.05f, radius * 1.0f); // パラメータはModelViewerを参考にした
 		return spotLightShadowView * spotLightShadowProj; // 行ベクトル形式の順序で乗算するのがXMMatrixMultiply()
 	}
