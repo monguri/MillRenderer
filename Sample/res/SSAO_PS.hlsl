a1f278b54c156ad697f605f1c1c13dc5dbf869ae
monguri <mongry@gmail.com>
2024-01-02 16:21:23 +0900

    USE_NORMALSの問題解消には至らなかったのでまたも深度だけの計算に戻しておく。 また、AORadiusInShaderが大きくてSSAOのサンプリングゴーストが見えていたので小さくした

--- Sample/res/SSAO_PS.hlsl
+++ Sample/res/SSAO_PS.hlsl
@@ -6,7 +6,7 @@
 
 #define USE_NORMALS 0
 #define USE_NORMALS_MONGURI 0
-#define USE_NORMALS_MONGURI2 1
+#define USE_NORMALS_MONGURI2 0
 
 #define SAMPLESET_ARRAY_SIZE 3
 static const float2 OcclusionSamplesOffsets[SAMPLESET_ARRAY_SIZE] =
@@ -19,7 +19,7 @@ static const float2 OcclusionSamplesOffsets[SAMPLESET_ARRAY_SIZE] =
 
 #define SAMPLE_STEPS 2
 
-static const float AORadiusInShader = 0.125f;
+static const float AORadiusInShader = 0.0042f; // divide UE's 0.0125f by 3. 0.0125f was too big.
 static const float AmbientOcclusionPower = 2.0f;
 static const float AmbientOcclusionIntensity = 0.5f;
 static const float AmbientOcclusionBias = 0.003f;
