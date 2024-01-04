94da4830172327bd3e3cafe0e4efb4a43135ab59
monguri <mongry@gmail.com>
2024-01-04 17:01:04 +0900

    SSAOの仮実装をしてみた。 シェーダコンパイルが通ったところでとりあえずコミット。

--- Sample/res/SSAOTestPS.hlsl
+++ Sample/res/SSAOTestPS.hlsl
@@ -13,6 +13,10 @@ static const float2 OcclusionSamplesOffsets[SAMPLESET_ARRAY_SIZE] =
 
 #define SAMPLE_STEPS 2
 
+static const float AO_RADIUS_IN_SS = 0.1f;
+static const float AO_POWER = 2.0f;
+static const float AO_INTENSITY = 0.5f;
+
 struct VSOutput
 {
 	float4 Position : SV_POSITION;
@@ -63,6 +67,29 @@ float3 ConverFromNDCToWS(float4 ndcPos)
 	return worldPos.xyz;
 }
 
+float3 WedgeWithNormal(float2 screenPos, float2 localRandom, float3 worldPos, float3 worldNormal)
+{
+	float2 screenPosL = screenPos + localRandom;
+	float2 screenPosR = screenPos - localRandom;
+
+	float deviceZ_L = DepthMap.Sample(PointClampSmp, screenPosL * float2(0.5f, -0.5f) + float2(0.5f, 0.5f)).r;
+	float deviceZ_R = DepthMap.Sample(PointClampSmp, screenPosR * float2(0.5f, -0.5f) + float2(0.5f, 0.5f)).r;
+
+	float4 ndcPosL = float4(screenPosL, deviceZ_L, 1);
+	float4 ndcPosR = float4(screenPosR, deviceZ_R, 1);
+	float3 worldPosL = ConverFromNDCToWS(ndcPosL);
+	float3 worldPosR = ConverFromNDCToWS(ndcPosR);
+
+	float3 deltaL = (worldPosL - worldPos);
+	float3 deltaR = (worldPosR - worldPos);
+
+	float invNormalAngleL = saturate(dot(deltaL, worldNormal) / dot(deltaL, deltaL));
+	float invNormalAngleR = saturate(dot(deltaR, worldNormal) / dot(deltaR, deltaR));
+	float weight = 1.0f;
+
+	return float3(invNormalAngleL, invNormalAngleR, weight);
+}
+
 float4 main(const VSOutput input) : SV_TARGET0
 {
 	float deviceZ = DepthMap.Sample(PointClampSmp, input.TexCoord).r;
@@ -77,7 +104,37 @@ float4 main(const VSOutput input) : SV_TARGET0
 	//// [-depth,depth]x[-depth,depth]x[near,far] i.e. view space pos.
 	//float3 viewSpacePosition = ConverFromSSPosToVSPos(screenPos, sceneDepth);
 
-	float result = 1.0f;
+#if 1
+	float2 rotation = float2(0, 1);
+#else
+	float2 rotation = (RandomNormalTex.Sample(PointWrapSmp, input.TexCoord * viewportUVtoRandomUV + TemporalOffset).rg * 2.0f - 1.0f);
+#endif
+
+	float2 weightAccumulator = 0.0001f;
+	// disk random loop
+	for (int i = 0; i < SAMPLESET_ARRAY_SIZE; i++)
+	{
+		float2 unrotatedRandom = OcclusionSamplesOffsets[i];
+		float2 localRandom = (unrotatedRandom.x * rotation + unrotatedRandom.y * float2(-rotation.y, rotation.x)) * AO_RADIUS_IN_SS;
+
+		float3 localAllumulator = 0.0f;
+
+		// ray-march loop
+		for (uint step = 0; step < SAMPLE_STEPS; step++)
+		{
+			float scale = (step + 1) / (float)SAMPLE_STEPS;
+
+			float3 stepSample = WedgeWithNormal(screenPos, scale * localRandom, worldPos, worldNormal);
+			localAllumulator = lerp(localAllumulator, float3(max(localAllumulator.xy, stepSample.xy), 1), stepSample.z);
+		}
+
+		weightAccumulator += float2((1 - localAllumulator.x) * (1 - localAllumulator.x) * localAllumulator.z, localAllumulator.z);
+		weightAccumulator += float2((1 - localAllumulator.y) * (1 - localAllumulator.y) * localAllumulator.z, localAllumulator.z);
+	}
+
+	float result = weightAccumulator.x / weightAccumulator.y;
+	// abs() to prevent shader warning
+	result = 1 - (1 - pow(abs(result), AO_POWER)) * AO_INTENSITY;
 
 	if (bEnableSSAO)
 	{
