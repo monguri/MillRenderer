8eb0d7bda0e3cdcfd4a08f2f2ee5307004ac05b0
monguri <mongry@gmail.com>
2024-01-08 16:38:19 +0900

    ダウンサンプルのロジックを実装した。

--- Sample/res/DownsamplePS.hlsl
+++ Sample/res/DownsamplePS.hlsl
@@ -15,6 +15,19 @@ SamplerState LinearClampMipPointSmp : register(s0);
 
 float4 main(const VSOutput input) : SV_TARGET0
 {
-	float4 result = SrcColorMap.Sample(LinearClampMipPointSmp, input.TexCoord);
-	return result;
+	float2 InvExtent = float2(1.0f / SrcWidth, 1.0f / SrcHeight);
+
+	float2 UVs[4];
+	UVs[0] = input.TexCoord + float2(-1, -1) * InvExtent;
+	UVs[1] = input.TexCoord + float2(1, -1) * InvExtent;
+	UVs[2] = input.TexCoord + float2(-1, 1) * InvExtent;
+	UVs[3] = input.TexCoord + float2(1, 1) * InvExtent;
+
+	float4 samples[4];
+	for (uint i = 0; i < 4; i++)
+	{
+		samples[i] = SrcColorMap.Sample(LinearClampMipPointSmp, UVs[i]);
+	}
+
+	return (samples[0] + samples[1] + samples[2] + samples[3]) * 0.25f;
 }
