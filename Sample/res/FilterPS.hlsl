fb7e6d378a41cb6d143e446cbdb817f74512fc9d
monguri <mongry@gmail.com>
2024-01-10 20:33:29 +0900

    フィルタシェーダを実装した。 未テスト。

--- Sample/res/FilterPS.hlsl
+++ Sample/res/FilterPS.hlsl
@@ -20,5 +20,19 @@ SamplerState LinearClampMipPointSmp : register(s0);
 
 float4 main(const VSOutput input) : SV_TARGET0
 {
-	return float4(1, 1, 1, 1);
+	float2 uv = input.TexCoord;
+
+
+	float3 accumColor = float3(0, 0, 0);
+	float accumWeight = 0;
+	for (int i = 0; i < NumSample; i++)
+	{
+		float weight = SampleWeights[i];
+		accumColor += SrcColorMap.Sample(LinearClampMipPointSmp, uv + SampleOffsets[i]).rgb * weight;
+		accumWeight += weight;
+	}
+
+	float3 filteredColor = accumColor / accumWeight;
+
+	return float4(filteredColor, 1);
 }
