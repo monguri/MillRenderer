b2f37e76203d2f33229a1374668312def3d2db64
monguri <mongry@gmail.com>
2024-02-10 20:20:17 +0900

    FXAAシェーダを一旦完成させた。 動作確認はまだ。

--- Sample/res/FXAA_PS.hlsl
+++ Sample/res/FXAA_PS.hlsl
@@ -1,7 +1,7 @@
 // used D3DSamples Fxaa3_11.hlsl default value.
-static const float SUB_PIX = 0.75f;
 static const float EDGE_THRESHOLD = 0.166f;
 static const float EDGE_THRESHOLD_MIN = 0.0833f;
+static const float EDGE_SHARPNESS = 8.0f;
 
 struct VSOutput
 {
@@ -26,9 +26,13 @@ float RGBtoLuma(float3 rgb)
 
 float4 main(const VSOutput input) : SV_TARGET0
 {
-	float2 rcpExtent = float2(1.0f / Width, 1.0f / Height);
-
 	float3 rgbM = ColorMap.Sample(PointClampSmp, input.TexCoord).rgb;
+	if (!bEnableFXAA)
+	{
+		return float4(rgbM, 1);
+	}
+
+	float2 rcpExtent = float2(1.0f / Width, 1.0f / Height);
 	float3 rgbNW = ColorMap.Sample(PointClampSmp, input.TexCoord + float2(-1, -1) * rcpExtent).rgb;
 	float3 rgbSW = ColorMap.Sample(PointClampSmp, input.TexCoord + float2(-1, +1) * rcpExtent).rgb;
 	float3 rgbNE = ColorMap.Sample(PointClampSmp, input.TexCoord + float2(+1, -1) * rcpExtent).rgb;
@@ -49,5 +53,31 @@ float4 main(const VSOutput input) : SV_TARGET0
 		return float4(rgbM, 1);
 	}
 
-	return float4(rgbM, 1);
+	float2 edgeDir;
+	edgeDir.x = -((lumaNW + lumaNE) - (lumaSW + lumaSE));
+	edgeDir.y = ((lumaNW + lumaSW) - (lumaNE + lumaSE));
+	edgeDir = normalize(edgeDir);
+
+	float3 rgbN1 = ColorMap.Sample(PointClampSmp, input.TexCoord - edgeDir * rcpExtent * 0.5f).rgb;
+	float3 rgbP1 = ColorMap.Sample(PointClampSmp, input.TexCoord + edgeDir * rcpExtent * 0.5f).rgb;
+	float3 rgbA = (rgbN1 + rgbP1) * 0.5f;
+
+	float edgeDirAbsMinTimesC = min(abs(edgeDir.x), abs(edgeDir.y)) * EDGE_SHARPNESS;
+	// edgeDirAbsMinTimesC cannot be 0. The case 0 is early returned.
+	// TODO:really?
+	edgeDir = clamp(edgeDir / edgeDirAbsMinTimesC, -2.0f, 2.0f);
+
+	float3 rgbN2 = ColorMap.Sample(PointClampSmp, input.TexCoord - edgeDir * rcpExtent * 2.0f).rgb;
+	float3 rgbP2 = ColorMap.Sample(PointClampSmp, input.TexCoord + edgeDir * rcpExtent * 2.0f).rgb;
+	float3 rgbB = (rgbN2 + rgbP2) * 0.25f + rgbA * 0.25f;
+
+	float lumaB = RGBtoLuma(rgbB);
+	if ((lumaB < lumaMin) || (lumaB > lumaMax))
+	{
+		return float4(rgbA, 1);
+	}
+	else
+	{
+		return float4(rgbB, 1);
+	}
 }
