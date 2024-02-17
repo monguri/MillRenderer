5918b43e75a9148318e6fbe36c435b0a9c4d2299
monguri <mongry@gmail.com>
2024-02-17 17:28:44 +0900

    ObjectVelocityパスのシェーダを実装した

--- Sample/res/VelocityPS.hlsl
+++ Sample/res/VelocityPS.hlsl
@@ -1,4 +1,13 @@
-float4 main() : SV_TARGET
+struct VSOutput
 {
-	return float4(1.0f, 1.0f, 1.0f, 1.0f);
+	float4 Position : SV_POSITION;
+	float2 CurClipPos : CUR_CLIP_POSITION;
+	float2 PrevClipPos : PREV_CLIP_POSITION;
+};
+
+float4 main(const VSOutput input) : SV_TARGET0
+{
+	float2 curUV = (input.CurClipPos + float2(1, -1)) * float2(0.5, -0.5);
+	float2 prevUV = (input.PrevClipPos + float2(1, -1)) * float2(0.5, -0.5);
+	return float4(curUV - prevUV, 0, 1);
 }
--- Sample/res/VelocityVS.hlsl
+++ Sample/res/VelocityVS.hlsl
@@ -1,4 +1,30 @@
-float4 main( float4 pos : POSITION ) : SV_POSITION
+struct VSInput
 {
-	return pos;
+	float3 Position : POSITION;
+	float3 Normal : NORMAL;
+};
+
+struct VSOutput
+{
+	float4 Position : SV_POSITION;
+	float2 CurClipPos : CUR_CLIP_POSITION;
+	float2 PrevClipPos : PREV_CLIP_POSITION;
+};
+
+cbuffer CbTransform : register(b0)
+{
+	float4x4 CurWVP : packoffset(c0);
+	float4x4 PrevWVP : packoffset(c4);
+}
+
+VSOutput main(VSInput input)
+{
+	VSOutput output = (VSOutput)0;
+
+	float4 localPos = float4(input.Position, 1.0f);
+	output.Position = mul(CurWVP, localPos);
+	output.CurClipPos = output.Position.xy;
+	output.PrevClipPos = mul(PrevWVP, localPos).xy;
+
+	return output;
 }
