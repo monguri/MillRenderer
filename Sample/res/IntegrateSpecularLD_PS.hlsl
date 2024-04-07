41bac05a9fcc06087668777029a17e9c9ae53c88
monguri <mongry@gmail.com>
2024-04-07 12:44:00 +0900

    計算をGLTF-Sample-Viewerのものに差し替えて見た。 結果はほぼ変わらないように見える

--- Sample/res/IntegrateSpecularLD_PS.hlsl
+++ Sample/res/IntegrateSpecularLD_PS.hlsl
@@ -62,11 +62,84 @@ float3 IntegrateSpecularCube(in float3 V, in float3 N, in float a, in float widt
 	}
 }
 
+// Referenced glTF-Sample-Viewer ibl_filtering.frag
+
+// Mipmap Filtered Samples (GPU Gems 3, 20.4)
+// https://developer.nvidia.com/gpugems/gpugems3/part-iii-rendering/chapter-20-gpu-based-importance-sampling
+// https://cgg.mff.cuni.cz/~jaroslav/papers/2007-sketch-fis/Final_sap_0073.pdf
+float computeLod(float pdf, int sampleCount)
+{
+    // // Solid angle of current sample -- bigger for less likely samples
+    // float omegaS = 1.0 / (float(u_sampleCount) * pdf);
+    // // Solid angle of texel
+    // // note: the factor of 4.0 * MATH_PI 
+    // float omegaP = 4.0 * MATH_PI / (6.0 * float(u_width) * float(u_width));
+    // // Mip level is determined by the ratio of our sample's solid angle to a texel's solid angle 
+    // // note that 0.5 * log2 is equivalent to log4
+    // float lod = 0.5 * log2(omegaS / omegaP);
+
+    // babylon introduces a factor of K (=4) to the solid angle ratio
+    // this helps to avoid undersampling the environment map
+    // this does not appear in the original formulation by Jaroslav Krivanek and Mark Colbert
+    // log4(4) == 1
+    // lod += 1.0;
+
+    // We achieved good results by using the original formulation from Krivanek & Colbert adapted to cubemaps
+
+    // https://cgg.mff.cuni.cz/~jaroslav/papers/2007-sketch-fis/Final_sap_0073.pdf
+	float lod = 0.5f * log2(6.0f * float(Width) / max(float(sampleCount) * pdf, 1e-8f));
+	return lod;
+}
+
+float3 filterColor(float3 N)
+{
+	float3 color = 0;
+	float weight = 0.0f;
+
+	float3 V = N;
+
+	int sampleCount = 512;
+	for (int i = 0; i < sampleCount; i++)
+	{
+		float4 importanceSample = GetImportanceSample(i, sampleCount, N, 0.0f);
+		float3 H = importanceSample.xyz;
+		float pdf = importanceSample.w;
+
+        // mipmap filtered samples (GPU Gems 3, 20.4)
+		float lod = computeLod(pdf, sampleCount);
+
+		// Note: reflect takes incident vector.
+		float3 L = normalize(reflect(-V, H));
+		float NdotL = dot(N, L);
+
+		if (NdotL > 0.0f)
+		{
+			if (Roughness == 0.0f)
+			{
+				// without this the roughness=0 lod is too high
+				lod = 0.0f;
+			}
+
+			float3 sampleColor = IBLCube.SampleLevel(IBLSmp, L, 0.0f).rgb;
+			color += sampleColor * NdotL;
+			weight += NdotL;
+		}
+	}
+
+	if (weight != 0.0f)
+	{
+		color /= weight;
+	}
+
+	return color;
+}
+
 float4 main(const VSOutput input) : SV_TARGET
 {
 	float3 output = 0;
 	float3 dir = CalcDirection(input.TexCoord, FaceIndex);
 
+#if 0
 	if (Roughness == 0.0f)
 	{
 		output = IBLCube.SampleLevel(IBLSmp, dir, 0.0f).rgb;
@@ -75,6 +148,10 @@ float4 main(const VSOutput input) : SV_TARGET
 	{
 		output = IntegrateSpecularCube(dir, dir, Roughness, Width, MipCount); 
 	}
+
+#else
+	output = filterColor(dir);
+#endif
 
 	return float4(output, 1.0f);
 }
