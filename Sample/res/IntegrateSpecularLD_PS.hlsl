a11ce871c3a6599df7aa63faf71d0789400fc4f5
monguri <mongry@gmail.com>
2024-04-07 12:49:59 +0900

    computeLodは共通で使うのでBakeUtil.hlsliに移動

--- Sample/res/IntegrateSpecularLD_PS.hlsl
+++ Sample/res/IntegrateSpecularLD_PS.hlsl
@@ -63,34 +63,6 @@ float3 IntegrateSpecularCube(in float3 V, in float3 N, in float a, in float widt
 }
 
 // Referenced glTF-Sample-Viewer ibl_filtering.frag
-
-// Mipmap Filtered Samples (GPU Gems 3, 20.4)
-// https://developer.nvidia.com/gpugems/gpugems3/part-iii-rendering/chapter-20-gpu-based-importance-sampling
-// https://cgg.mff.cuni.cz/~jaroslav/papers/2007-sketch-fis/Final_sap_0073.pdf
-float computeLod(float pdf, int sampleCount)
-{
-    // // Solid angle of current sample -- bigger for less likely samples
-    // float omegaS = 1.0 / (float(u_sampleCount) * pdf);
-    // // Solid angle of texel
-    // // note: the factor of 4.0 * MATH_PI 
-    // float omegaP = 4.0 * MATH_PI / (6.0 * float(u_width) * float(u_width));
-    // // Mip level is determined by the ratio of our sample's solid angle to a texel's solid angle 
-    // // note that 0.5 * log2 is equivalent to log4
-    // float lod = 0.5 * log2(omegaS / omegaP);
-
-    // babylon introduces a factor of K (=4) to the solid angle ratio
-    // this helps to avoid undersampling the environment map
-    // this does not appear in the original formulation by Jaroslav Krivanek and Mark Colbert
-    // log4(4) == 1
-    // lod += 1.0;
-
-    // We achieved good results by using the original formulation from Krivanek & Colbert adapted to cubemaps
-
-    // https://cgg.mff.cuni.cz/~jaroslav/papers/2007-sketch-fis/Final_sap_0073.pdf
-	float lod = 0.5f * log2(6.0f * float(Width) / max(float(sampleCount) * pdf, 1e-8f));
-	return lod;
-}
-
 float3 filterColor(float3 N)
 {
 	float3 color = 0;
@@ -106,7 +78,7 @@ float3 filterColor(float3 N)
 		float pdf = importanceSample.w;
 
         // mipmap filtered samples (GPU Gems 3, 20.4)
-		float lod = computeLod(pdf, sampleCount);
+		float lod = computeLod(pdf, sampleCount, Width);
 
 		// Note: reflect takes incident vector.
 		float3 L = normalize(reflect(-V, H));
