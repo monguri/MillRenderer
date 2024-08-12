c60ef9e383a4f803ada11ef8cea32fdc48614ae6
monguri <mongry@gmail.com>
2024-08-12 20:15:18 +0900

    シェーダの実装途中だが保存のためコミット

--- Sample/res/SkyTransmittanceLUT_CS.hlsl
+++ Sample/res/SkyTransmittanceLUT_CS.hlsl
@@ -32,6 +32,50 @@ void UVtoLUTTransmittanceParams(out float viewHeight, out float viewZenithCosAng
 	viewZenithCosAngle = clamp(viewZenithCosAngle, -1.0f, 1.0f);
 }
 
+struct SingleScatteringResult
+{
+	float3 opticalDepth; // Optical depth (1/m)
+};
+
+struct SamplingSetup
+{
+	bool valiableSampleCount;
+	float sampleCountIni; // Used when VariableSampleCount is false
+};
+
+// In this function, all world position are relative to the planet center (itself expressed within translated world space)
+SingleScatteringResult IntegrateSingleScatteredLuminance(
+	in float4 SVPos, in float3 worldPos, in float3 worldDir,
+	in SamplingSetup sampling
+)
+{
+	SingleScatteringResult result;
+	result.opticalDepth = 0;
+
+	if (dot(worldPos, worldPos) <= bottomRadiusKm * bottomRadiusKm)
+	{
+		return result; // Camera is inside the planet ground
+	}
+
+	float3 opticalDepth = 0.0f;
+
+
+	// TODO:impl
+	float tMax = 0.0f;
+
+	float sampleCount = sampling.sampleCountIni;
+	float sampleCountFloor = sampling.sampleCountIni;
+	float t = 0.0f;
+
+	float3 pixelNoise = 0.3f; // from UE's DEFAULT_SAMPLE_OFFSET 
+	for (float sampleI = 0.0f; sampleI < sampleCount; sampleI += 1.0f)
+	{
+	}
+
+	result.opticalDepth = opticalDepth;
+	return result;
+}
+
 [RootSignature(ROOT_SIGNATURE)]
 [numthreads(TILE_PIXEL_SIZE_X, TILE_PIXEL_SIZE_Y, 1)]
 void main(uint2 DTid : SV_DispatchThreadID)
@@ -46,6 +90,12 @@ void main(uint2 DTid : SV_DispatchThreadID)
 
 	float3 worldPos = float3(0.0f, 0.0f, viewHeight);
 	float3 worldDir = float3(0.0f, sqrt(1.0f - viewZenithCosAngle * viewZenithCosAngle), viewZenithCosAngle);
+
+	SamplingSetup sampling = (SamplingSetup)0;
+	{
+		sampling.valiableSampleCount = false;
+		sampling.sampleCountIni = 10.0f;
+	}
 
 	const bool ground = false;
 
