4f93790ad79b2df7eb80db2e5dd04a6b26bc14f2
monguri <mongry@gmail.com>
2024-09-28 10:12:25 +0900

    エントリポイントを追加

---
+++ ParticleSample/src/main.cpp
@@ -0,0 +1,13 @@
+#if defined(DEBUG) || defined(_DEBUG)
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
+#endif
+
+int wmain(int argc, wchar_t** argv, wchar_t** evnp)
+{
+#if defined(DEBUG) || defined(_DEBUG)
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+    return 0;
+}
