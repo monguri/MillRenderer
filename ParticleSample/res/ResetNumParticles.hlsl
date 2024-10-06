b9fb065c645a147da37e5d94541178ac6f75272a
monguri <mongry@gmail.com>
2024-10-06 10:02:27 +0900

    ファイル改名。 またRootSignatureをシェーダ内記述に。

--- ParticleSample/res/ResetNumParticles.hlsl
+++ ParticleSample/res/ResetNumParticles.hlsl
@@ -1,7 +1,11 @@
+#define ROOT_SIGNATURE ""\
+"DescriptorTable(UAV(u0))"\
+
 static const uint BYTE_OFFSET_INSTANCE_COUNT = 4;
 
-RWByteAddressBuffer CurrDrawParticlesIndirectArgs : register(u1);
+RWByteAddressBuffer CurrDrawParticlesIndirectArgs : register(u0);
 
+[RootSignature(ROOT_SIGNATURE)]
 [numthreads(1, 1, 1)]
 void main( uint3 DTid : SV_DispatchThreadID )
 {
