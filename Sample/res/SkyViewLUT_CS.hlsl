68d53c7b4be1334533669d2dec31a596bb6f5472
monguri <mongry@gmail.com>
2024-11-23 13:31:21 +0900

    CSのひな型を追加

---
+++ Sample/res/SkyViewLUT_CS.hlsl
@@ -0,0 +1,33 @@
+#include "SkyCommon.hlsli"
+
+#define ROOT_SIGNATURE ""\
+"DescriptorTable(CBV(b0))"\
+", DescriptorTable(SRV(t0))"\
+", DescriptorTable(UAV(u0))"\
+", StaticSampler"\
+"("\
+"s0"\
+", filter = FILTER_MIN_MAG_MIP_LINEAR"\
+", addressU = TEXTURE_ADDRESS_CLAMP"\
+", addressV = TEXTURE_ADDRESS_CLAMP"\
+", addressW = TEXTURE_ADDRESS_CLAMP"\
+", maxAnisotropy = 1"\
+", comparisonFunc = COMPARISON_NEVER"\
+", borderColor = STATIC_BORDER_COLOR_TRANSPARENT_BLACK"\
+")"\
+
+Texture2D MultiScatteredLuminaceLutTexture : register(t0);
+RWTexture2D<float3> OutResult : register(u0);
+
+static const uint TILE_PIXEL_SIZE_X = 8;
+static const uint TILE_PIXEL_SIZE_Y = 8;
+
+[RootSignature(ROOT_SIGNATURE)]
+[numthreads(TILE_PIXEL_SIZE_X, TILE_PIXEL_SIZE_Y, 1)]
+void main(uint2 DTid : SV_DispatchThreadID)
+{
+	float2 pixPos = DTid + 0.5f;
+	float2 uv = pixPos / float2(MultiScatteringLUT_Width, MultiScatteringLUT_Height);
+
+	OutResult[int2(pixPos)] = MultiScatteredLuminaceLutTexture.Sample Level(LinearClampSampler, uv, 0).rgb;
+}
