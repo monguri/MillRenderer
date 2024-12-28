9eeae4ebe339f7667d0a8d67218a22966d97e665
monguri <mongry@gmail.com>
2024-12-28 10:59:55 +0900

    SkyBoxはNormalとMetallicRoughnessの書き込みをしてないので0扱いになっていたが、それだとSSRにおいて法線が(-1,-1,-1)扱いになり、roughnessも0なので面によって反射で別の色がのってしまう。 よってroughessを1にしてSSRがのらないようにした

--- Sample/res/SkyBoxPS.hlsl
+++ Sample/res/SkyBoxPS.hlsl
@@ -4,10 +4,29 @@ struct VSOutput
 	float3 TexCoord : TEXCOORD;
 };
 
+struct PSOutput
+{
+	float4 Color : SV_TARGET0;
+	float4 Normal : SV_TARGET1;
+	float2 MetallicRoughness : SV_TARGET2;
+};
+
 TextureCube CubeMap : register(t0);
 SamplerState CubeSmp : register(s0);
 
-float4 main(const VSOutput input) : SV_TARGET0
+PSOutput main(const VSOutput input)
 {
-	return CubeMap.SampleLevel(CubeSmp, input.TexCoord, 0);
+	PSOutput output;
+
+	output.Color = CubeMap.SampleLevel(CubeSmp, input.TexCoord, 0);
+
+	// 法線は(0, 0, 0)扱い
+	output.Normal.xyz = 0.5f;
+	output.Normal.a = 1;
+
+	// 反射を起こさないようにメタリックは0、roughnessは1
+	output.MetallicRoughness.r = 0;
+	output.MetallicRoughness.g = 1;
+
+	return output;
 }
