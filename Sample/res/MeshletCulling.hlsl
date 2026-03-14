cb9fb8ea9015e02bc86b68e3a051ffc2dcb89bd7
monguri <mongry@gmail.com>
2026-03-14 13:00:19 +0900

    フラスタムカリングの実装

--- Sample/res/MeshletCulling.hlsl
+++ Sample/res/MeshletCulling.hlsl
@@ -1,4 +1,117 @@
+#define ROOT_SIGNATURE ""\
+"RootFlags"\
+"("\
+"DENY_VERTEX_SHADER_ROOT_ACCESS"\
+" | DENY_PIXEL_SHADER_ROOT_ACCESS"\
+" | DENY_HULL_SHADER_ROOT_ACCESS"\
+" | DENY_DOMAIN_SHADER_ROOT_ACCESS"\
+" | DENY_GEOMETRY_SHADER_ROOT_ACCESS"\
+" | DENY_AMPLIFICATION_SHADER_ROOT_ACCESS"\
+" | DENY_MESH_SHADER_ROOT_ACCESS"\
+")"\
+", DescriptorTable(CBV(b0), visibility = SHADER_VISIBILITY_ALL)"\
+", DescriptorTable(CBV(b1), visibility = SHADER_VISIBILITY_ALL)"\
+", DescriptorTable(SRV(t0), visibility = SHADER_VISIBILITY_ALL)"\
+", DescriptorTable(UAV(u0), visibility = SHADER_VISIBILITY_ALL)"\
+", DescriptorTable(UAV(u1), visibility = SHADER_VISIBILITY_ALL)"\
+
+
+struct Transform
+{
+	float4x4 ViewProj;
+	float4x4 WorldToDirLightShadowMap;
+	float4x4 WorldToSpotLight1ShadowMap;
+	float4x4 WorldToSpotLight2ShadowMap;
+	float4x4 WorldToSpotLight3ShadowMap;
+};
+
+struct Mesh
+{
+	float4x4 World;
+	uint MeshIdx;
+};
+
+struct AABB
+{
+	float3 Center;
+	float3 HalfExtent;
+};
+
+struct DispatchIndirectArgs
+{
+	uint ThreadGroupCountX;
+	uint ThreadGroupCountY;
+	uint ThreadGroupCountZ;
+};
+
+ConstantBuffer<Transform> CbTransform : register(b0);
+ConstantBuffer<Mesh> CbMesh : register(b1);
+StructuredBuffer<AABB> SbAABBInfos : register(t0);
+RWStructuredBuffer<DispatchIndirectArgs> DrawVBufferIndirectArgSB : register(u0);
+RWStructuredBuffer<uint> DrawVBufferMeshletListSB : register(u1);
+
+// InverseZ、InfinitePlane
+bool frustumCull(float3 aabbNdcPos[8])
+{
+	//TODO: AABBがフラスタムを囲むように交差する場合は考慮してない
+
+	// AABBの8頂点のうち、1つでもフラスタム内にあれば交差している
+	bool isIntersecting = false;
+	for (uint i = 0; i < 8; i++)
+	{
+		if (aabbNdcPos[i].x < -1.0f || aabbNdcPos[i].x > 1.0f)
+		{
+			continue;
+		}
+		
+		if (aabbNdcPos[i].y < -1.0f || aabbNdcPos[i].y > 1.0f)
+		{
+			continue;
+		}
+		
+		if (aabbNdcPos[i].z < 0.0f || aabbNdcPos[i].z > 1.0f)
+		{
+			continue;
+		}
+
+		isIntersecting = true;
+	}
+
+	return isIntersecting;
+}
+
+[RootSignature(ROOT_SIGNATURE)]
 [numthreads(64, 1, 1)]
 void main(uint meshletIdx : SV_DispatchThreadID)
 {
+	AABB aabb = SbAABBInfos[meshletIdx];
+
+	float3 vertices[8] =
+	{
+		aabb.Center + float3(-aabb.HalfExtent.x, -aabb.HalfExtent.y, -aabb.HalfExtent.z),
+		aabb.Center + float3(-aabb.HalfExtent.x, -aabb.HalfExtent.y,  aabb.HalfExtent.z),
+		aabb.Center + float3(-aabb.HalfExtent.x,  aabb.HalfExtent.y, -aabb.HalfExtent.z),
+		aabb.Center + float3(-aabb.HalfExtent.x,  aabb.HalfExtent.y,  aabb.HalfExtent.z),
+		aabb.Center + float3( aabb.HalfExtent.x, -aabb.HalfExtent.y, -aabb.HalfExtent.z),
+		aabb.Center + float3( aabb.HalfExtent.x, -aabb.HalfExtent.y,  aabb.HalfExtent.z),
+		aabb.Center + float3( aabb.HalfExtent.x,  aabb.HalfExtent.y, -aabb.HalfExtent.z),
+		aabb.Center + float3( aabb.HalfExtent.x,  aabb.HalfExtent.y,  aabb.HalfExtent.z),
+	};
+
+	// モデル座標からNDC座標への変換
+	for (uint i = 0; i < 8; i++)
+	{
+		float4 clipPos = mul(CbTransform.ViewProj, mul(CbMesh.World, float4(vertices[i], 1.0f)));
+		vertices[i] = clipPos.xyz / clipPos.w;
+	}
+
+	bool visible = frustumCull(vertices);
+
+	if (visible)
+	{
+		uint visibleMeshletIdx;
+		InterlockedAdd(DrawVBufferIndirectArgSB[0].ThreadGroupCountX, 1, visibleMeshletIdx);
+
+		DrawVBufferMeshletListSB[visibleMeshletIdx] = meshletIdx;
+	}
 }
